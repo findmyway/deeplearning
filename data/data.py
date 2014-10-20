407dfae357cef734c7fbcce17af82fb3322d8d65
TianJun <find_my_way@foxmail.com>
2014-10-20 16:25:10 +0800

    transform original int data into float

--- data/data.py
+++ data/data.py
@@ -1,9 +1,10 @@
 import os
-
 import numpy as np
 
 curpath = os.path.dirname(__file__)
-x_train = np.load(curpath + '/x_train.npy')
-x_test = np.load(curpath + '/x_test.npy')
+x_train_int = np.load(curpath + '/x_train.npy')
+x_train = x_train_int / 255.0
+x_test_int = np.load(curpath + '/x_test.npy')
+x_test = x_test_int / 255.0
 y_train = np.load(curpath + '/y_train.npy')
 y_test = np.load(curpath + '/y_test.npy')
